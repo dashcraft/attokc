adeb61797f5d1eb2b58b456abaf2d68331de4ea9
dashcraft <ashcraftbookkeeping@gmail.com>
2017-03-07 19:38:45 -0600

    corrected footer, made navbar responseive, still need to make a mobile navbar, integrated a instagram feed to access images, adjusted css for instagram feed

--- public/js/custom.js
+++ public/js/custom.js
@@ -26,5 +26,18 @@ $(document).ready(function(){
 		}
 	})
 
+	//	<span>{{likes}}{{comments}}</span> will put instagram photo comment count!!!
+	var userFeed = new Instafeed({
+	    get: 'user',
+	    userId:'4777661928',
+	    clientId: '6cf789de42554f54903d08e66c2c0ef7 ',
+	    accessToken: '4777661928.1677ed0.fe1bc3b9d9564a5391907d0190f16441',
+	    resolution: 'standard_resolution',
+	    template: '<div class="instafeed_img"><a href="{{link}}" target="_blank" id="{{id}}"><img src="{{image}}" /></a></div>',
+	    sortBy: 'most-recent',
+	    limit: 9,
+	    links: true
+	 });
+  	userFeed.run();
 
 })
