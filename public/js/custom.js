7b80cfa21fd5e08b0b0e2aac8de2f742e075858a
dashcraft <ashcraftbookkeeping@gmail.com>
2017-03-08 19:26:08 -0600

    Add some blog stuff, need to make blog items clear rows, may have rawle use bootstrap for that...

--- public/js/custom.js
+++ public/js/custom.js
@@ -3,15 +3,14 @@ $(document).ready(function(){
 
 
 	$('#toggle-dropdown').on('click',function(){
+		
 		if(!$('.dropdown-menu-two').hasClass('hidden')){
 			$('.dropdown-menu-two').toggleClass('hidden');
 		}
-		console.log('toggled');
 		$('.dropdown-menu').toggleClass('hidden');
 	});
 
 	$('#toggle-dropdown-two').on('click',function(){
-		console.log('toggled');
 		if(!$('.dropdown-menu').hasClass('hidden')){
 			$('.dropdown-menu').toggleClass('hidden');
 		}
--- routes/views/blog.js
+++ routes/views/blog.js
@@ -59,8 +59,8 @@ exports = module.exports = function (req, res) {
 
 		var q = keystone.list('Post').paginate({
 			page: req.query.page || 1,
-			perPage: 10,
-			maxPages: 10,
+			perPage: 8,
+			maxPages: 20,
 			filters: {
 				state: 'published',
 			},
